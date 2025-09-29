ae8c23fca9730ec1c2c0c551e1d92245ea4a223c
Mar Simó Alejos <malejos@labster.com>
2025-09-29 22:16:21 +0200

    Primer commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const map = L.map('leaflet-map').setView([41.804, 1.823], 15);
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+L.marker([41.804, 1.823]).addTo(map)
+  .bindPopup('Castillo de Cardona')
+  .openPopup();
