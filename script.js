2d1bbec27d0a13e70731de7f35b224698abb7f55
Mar Simó Alejos <astraproductionscontact@gmail.com>
2025-09-30 09:42:26 +0200

    details

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const map = L.map('leaflet-map').setView([41.804, 1.823], 15);
 
+document.getElementById("book-btn").addEventListener("click", () => {
+    window.location.href = "src/components/booking.html";
+});
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
